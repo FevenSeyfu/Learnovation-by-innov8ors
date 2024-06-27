d4e458ed8ccdb2279a8797f476b0041280592664
FevenSeyfu <fevenseyfu00@gmail.com>
2024-06-27 03:01:43 +0300

    Add custom font

--- Frontend/src/components/LandingPage/Home/Hero.jsx
+++ Frontend/src/components/LandingPage/Home/Hero.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import heroImgDesktop from "../../../assets/images/hero-img-desktop.png";
+import heroImgMobile from "../../../assets/images/hero-img-mobile.png";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="w-full flex flex-col-reverse md:flex-row mt-[27px] md:mt-[130px]">
+      <div>
+        <h1 className="">Free certificates & courses for online learners</h1>
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit interdum
+          ullamcorper sed pharetra sene.
+        </p>
+        <button>Get Started</button>
+      </div>
+      <picture>
+        <source media="(min-width: 375px)" srcset={heroImgDesktop} />
+        <img src={heroImgMobile} alt="hero image" />
+      </picture>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
