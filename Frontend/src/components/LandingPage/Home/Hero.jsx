ec5d3be95a36203cd98fa27442547fc9b789c586
FevenSeyfu <fevenseyfu00@gmail.com>
2024-06-27 23:02:33 +0300

    complete homepage hero section

--- Frontend/src/components/LandingPage/Home/Hero.jsx
+++ Frontend/src/components/LandingPage/Home/Hero.jsx
@@ -4,20 +4,20 @@ import heroImgMobile from "../../../assets/images/hero-img-mobile.png";
 
 const Hero = () => {
   return (
-    <div className="w-full flex flex-col-reverse md:flex-row mt-[27px] md:mt-[130px]">
-      <div>
-        <h1 className="">Free certificates & courses for online learners</h1>
-        <p className="">
+    <div className="w-full flex flex-col-reverse md:flex-row items-center justify-center mt-[27px] md:mt-[130px]  md:mx-[10%] md:w-[80%] gap-7">
+      <div className="flex flex-col w-full px-8 text-left gap-2 font-nunito">
+        <h1 className=" md:font-inter text-4xl font-extrabold md:font-semibold md:text-[75px] leading-tight text-[#242331]">Free certificates & courses for online learners</h1>
+        <p className="text-2xl font-normal text-[#797979] leading-8">
           Lorem ipsum dolor sit amet consectetur adipiscing elit interdum
           ullamcorper sed pharetra sene.
         </p>
-        <button>Get Started</button>
+        <button className="w-full md:w-[192px] font-inter inline-block rounded-lg py-4 bg-gradient-to-r from-indigo-500 via-fusciaPink to-lightPink  font-bold text-center text-2xl text-white mt-8">Get Started</button>
       </div>
-      <picture>
-        <source media="(min-width: 375px)" srcset={heroImgDesktop} />
+      <picture className="w-full">
+        <source media="(min-width: 375px)" srcSet={heroImgDesktop} />
         <img src={heroImgMobile} alt="hero image" />
       </picture>
-    </div>
+    </div> 
   );
 };
 
