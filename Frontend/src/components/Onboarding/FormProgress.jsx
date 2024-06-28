c078e6d8a1745dc4315bdc5c4b3d8fe6b098a3d5
FevenSeyfu <fevenseyfu00@gmail.com>
2024-06-28 18:58:40 +0300

    create page,component and json data for onboarding form

---
+++ Frontend/src/components/Onboarding/FormProgress.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormProgress = () => {
+  return (
+    <div>FormProgress</div>
+  )
+}
+
+export default FormProgress
---
+++ Frontend/src/components/Onboarding/FormStep.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormStep = () => {
+  return (
+    <div>FormStep</div>
+  )
+}
+
+export default FormStep
