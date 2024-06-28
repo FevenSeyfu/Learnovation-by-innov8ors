ea8dd3483942f75a506fbf0f31fca3755ad696e0
FES001 <firdavs7794948@gmail.com>
2024-06-29 03:25:22 +0500

    small changes

--- Frontend/src/components/Explore/script.js
+++ Frontend/src/components/Explore/script.js
@@ -3,8 +3,8 @@
 const sidebarAnchors = document.querySelectorAll('.sidebar-anchor');
 
 
-for (const sidebarAnchor of sidebarAnchors) {
+/* for (const sidebarAnchor of sidebarAnchors) {
     sidebarAnchor.addEventListener('click', () => {
         sidebarAnchor.classList.remove('opened-anchor');
     });
-};
+}; */
