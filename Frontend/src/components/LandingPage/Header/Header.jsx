89db218620f0ece653ec926555044997a86df211
Arnav Bhalgat <148126694+bhalgatarnav@users.noreply.github.com>
2024-06-29 00:19:03 +0530

    Updated Header Page.

--- Frontend/src/components/LandingPage/Header/Header.jsx
+++ Frontend/src/components/LandingPage/Header/Header.jsx
@@ -5,26 +5,20 @@ const Header = () => {
   return (
       <header className="bg-white py-4 shadow-md">
         <div className="container mx-auto flex justify-between items-center">
-          <div className="flex items-center"> {/* Wrapper for logo and text */}
-            <img src="/LearnovationAcademyLogo.png" alt="Learnnovation Academy Logo" className="h-16 mr-4" />
-            <span className="font-bold text-lg">LEARNOVATION ACADEMY</span>
-          </div>
+          <Link to="/" className="flex items-center"> {/* Link for the logo */}
+            <img src="/LearnovationAcademyLogo.png" alt="Learnnovation Academy Logo" className="h-20" />
+          </Link>
 
           <nav className="flex items-center">
-            <ul className="flex space-x-6">
-              <li>
-                <Link to="/" className="text-blue-500 hover:text-blue-700">Home</Link>
-              </li>
+            <ul className="flex space-x-6 text-lg font-medium items-center">
               <li>
                 <Link to="/contact" className="text-gray-700 hover:text-gray-900">Contact</Link>
               </li>
               <li>
-                <Link to="/login" className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                  Sign In
-                </Link>
+                <Link to="/login" className="text-gray-700 hover:text-gray-900 font-bold">Log In</Link>
               </li>
               <li>
-                <Link to="/signup" className="bg-violet-500 hover:bg-violet-700 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                <Link to="/signup" className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline"> {/* Link for Sign Up button */}
                   Sign Up
                 </Link>
               </li>
