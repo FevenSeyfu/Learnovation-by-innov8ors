903baad0fca7f34f09dc93a962398283a85b85aa
Feven Seyfu <fevenseyfu00@gmail.com>
2024-06-29 02:28:50 +0300

    Footer page (#10)
    * Header Code
    
    * Updated Header Page.
    
    * Footer Code
    
    * Footer Code
    
    * Refactoring
    
    * Icons
    
    * All
    
    * Resolving Conflicts for Header.
    
    * Update Footer.jsx
    
    White background
    
    * delete unused file
    
    * Remove ignored files
    
    ---------
    
    Co-authored-by: Arnav Bhalgat <148126694+bhalgatarnav@users.noreply.github.com>

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,21 +8,21 @@ import Login from "./Pages/Authentication/Login";
 import SignUp from "./Pages/Authentication/SignUp";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* landing page */}
-          <Route index element={<HomePage />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-          {/* Authentication  */}
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    {/* landing page */}
+                    <Route index element={<HomePage />} />
+                    <Route path="/contact" element={<ContactUs />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+                {/* Authentication  */}
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- Frontend/src/components/LandingPage/Header/Header.jsx
+++ Frontend/src/components/LandingPage/Header/Header.jsx
@@ -1,26 +1,41 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <nav>
-        <ul className="flex flex-row justify-between">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign up</Link>
-          </li>
-          <li>
-            <Link to="/login">Log in</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+      <header className="bg-white py-4 shadow-md">
+        <div className="container mx-auto flex justify-between items-center">
+          <Link to="/" className="flex items-center"> {/* Link for the logo */}
+            <img src="/LearnovationAcademyLogo.png" alt="Learnnovation Academy Logo" className="h-20" />
+          </Link>
+            <nav className="flex items-center space-x-4">
+                  <ul className="flex space-x-4 text-lg font-medium items-center">
+                      <li>
+                          <Link to="/features" className="text-gray-700 hover:text-gray-900">Features</Link>
+                      </li>
+                      <li>
+                          <Link to="/contact" className="text-gray-700 hover:text-gray-900">Contact</Link>
+                      </li>
+                  </ul>
+
+                  <ul className="flex space-x-8 text-lg font-medium items-center">
+
+                  </ul>
+
+                  <div className="flex items-center space-x-4"> {/* Increased spacing with space-x-8 */}
+                      <Link to="/signup"
+                              className="bg-purple hover:opacity-95 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                          Sign Up
+                      </Link>
+                      <Link to="/login"
+                              className="bg-white hover:bg-gray-100 text-gray-700 font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                            Log In
+                      </Link>
+                  </div>
+              </nav>
+          </div>
+      </header>
   );
 
 };
