7cd24f12ff7dd2986438f30213d0be705346ad79
FevenSeyfu <fevenseyfu00@gmail.com>
2024-06-28 04:31:58 +0300

    complete the homepage call to action section

--- Frontend/src/components/LandingPage/Home/Hero.jsx
+++ Frontend/src/components/LandingPage/Home/Hero.jsx
@@ -4,8 +4,8 @@ import ButtonSm from "../../utility/ButtonSm";
 
 const Hero = () => {
   return (
-    <div className="h-screen w-full flex flex-col-reverse md:flex-row items-center justify-center   md:mx-[10%] md:w-[80%] gap-7">
-      <div className="w-full md:w-1/2 flex flex-col px-8 text-left gap-2 font-nunito">
+    <div className="h-screen w-full flex flex-col-reverse md:flex-row items-center justify-center px-6  md:mx-[10%] md:w-[80%] gap-7">
+      <section className="w-full md:w-1/2 flex flex-col px-2 text-left gap-2 font-nunito">
         <h1 className=" md:font-inter text-4xl font-extrabold md:font-semibold  md:text-5xl lg:text-6xl leading-tight text-darkGray">
           Free certificates & courses for online learners
         </h1>
@@ -14,7 +14,7 @@ const Hero = () => {
           ullamcorper sed pharetra sene.
         </p>
         <ButtonSm>Get Started</ButtonSm>
-      </div>
+      </section>
       <img src={heroImg} alt="hero image" className="w-full md:w-1/2 h-auto" />
     </div>
   );
