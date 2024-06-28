30e7c09c83f913bf219d201e964c80c4e1db10f9
Arnav Bhalgat <148126694+bhalgatarnav@users.noreply.github.com>
2024-06-29 02:42:08 +0530

    Footer Code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,32 +1,28 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/LandingPage/Layout";
-import NotFound from "./components/Pages/NotFound/NotFound";
-import HomePage from "./components/Pages/LandingPage/HomePage";
-import ContactUs from "./components/Pages/LandingPage/ContactUs";
-import Login from "./components/Pages/Authentication/Login";
-import SignUp from "./components/Pages/Authentication/SignUp";
-import Header from "./Header"; // Import your Header component
+import NotFound from "./Pages/NotFound/NotFound";
+import HomePage from "./Pages/LandingPage/HomePage";
+import ContactUs from "./Pages/ContactUs/ContactUs";
+import Login from "./Pages/Authentication/Login";
+import SignUp from "./Pages/Authentication/SignUp";
 
 function App() {
-  return (
-      <BrowserRouter>
-        <div> {/* Add a div to wrap the header */}
-          <Header /> {/* Render the Header component */}
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              {/* landing page */}
-              <Route index element={<HomePage />} />
-              <Route path="/contact" element={<ContactUs />} />
-              <Route path="*" element={<NotFound />} />
-            </Route>
-            {/* Authentication  */}
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    {/* landing page */}
+                    <Route index element={<HomePage />} />
+                    <Route path="/contact" element={<ContactUs />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+                {/* Authentication  */}
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
