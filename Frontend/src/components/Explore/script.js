c2ac511d409598c3ca8072abda7b46cab2a986a7
FES001 <firdavs7794948@gmail.com>
2024-06-29 03:15:16 +0500

    s

---
+++ Frontend/src/components/Explore/script.js
@@ -0,0 +1,10 @@
+// SIDEBAR
+
+const sidebarAnchors = document.querySelectorAll('.sidebar-anchor');
+
+
+for (const sidebarAnchor of sidebarAnchors) {
+    sidebarAnchor.addEventListener('click', () => {
+        sidebarAnchor.classList.remove('opened-anchor');
+    });
+};
