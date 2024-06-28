d79c040ab3b3683d8870ee46ae8c7fd9bef78472
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2024-06-28 15:05:54 -0500

    dashboard header changes

--- Frontend/src/components/Dashboard/Header/Header.jsx
+++ Frontend/src/components/Dashboard/Header/Header.jsx
@@ -2,26 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div>
-      <nav>
-        <ul className="flex flex-row justify-between">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign up</Link>
-          </li>
-          <li>
-            <Link to="/login">Log in</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+    return (
+        <div>
+            <nav>
+                <ul className="flex flex-row justify-between">
+
+                    <Form inline>
+                        <FormControl
+                            onChange={this.handleSearchInput}
+                            value={this.state.searchText}
+                            type="text"
+                            placeholder="Search"
+                            className="mr-sm-2"
+                        />
+                        <Button onClick={this.handleSearchSubmit} variant="outline-info">
+                            Search
+                        </Button>
+                    </Form>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/signup">Sign up</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Log in</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
 };
 
 export default Header;
