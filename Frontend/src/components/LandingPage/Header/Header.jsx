c94b033496d3e89789aa1fc419616983fcb07af8
Arnav Bhalgat <148126694+bhalgatarnav@users.noreply.github.com>
2024-06-29 04:25:50 +0530

    Update Header.jsx

--- Frontend/src/components/LandingPage/Header/Header.jsx
+++ Frontend/src/components/LandingPage/Header/Header.jsx
@@ -9,26 +9,34 @@ const Header = () => {
           <Link to="/" className="flex items-center"> {/* Link for the logo */}
             <img src="/LearnovationAcademyLogo.png" alt="Learnnovation Academy Logo" className="h-20" />
           </Link>
+            <nav className="flex items-center space-x-4">
+                  <ul className="flex space-x-4 text-lg font-medium items-center">
+                      <li>
+                          <Link to="/features" className="text-gray-700 hover:text-gray-900">Features</Link>
+                      </li>
+                      <li>
+                          <Link to="/contact" className="text-gray-700 hover:text-gray-900">Contact</Link>
+                      </li>
+                  </ul>
 
-          <nav className="flex items-center">
-            <ul className="flex space-x-6 text-lg font-medium items-center">
-              <li>
-                <Link to="/contact" className="text-gray-700 hover:text-gray-900">Contact</Link>
-              </li>
-              <li>
-                <Link to="/login" className="text-gray-700 hover:text-gray-900 font-bold">Log In</Link>
-              </li>
-              <li>
-                <Link to="/signup" className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline"> {/* Link for Sign Up button */}
-                  Sign Up
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
+                  <ul className="flex space-x-8 text-lg font-medium items-center">
+
+                  </ul>
+
+                  <div className="flex items-center space-x-4"> {/* Increased spacing with space-x-8 */}
+                      <Link to="/signup"
+                              className="bg-purple-500 hover:bg-purple-700 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                          Sign Up
+                      </Link>
+                      <Link to="/login"
+                              className="bg-white hover:bg-gray-100 text-gray-700 font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                            Log In
+                      </Link>
+                  </div>
+              </nav>
+          </div>
       </header>
   );
-
 };
 
 export default Header;
