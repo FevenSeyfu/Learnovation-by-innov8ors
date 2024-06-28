4f1a8b3cc78749c6e3aa552fc469c36fbc1d5c54
FevenSeyfu <fevenseyfu00@gmail.com>
2024-06-28 04:22:52 +0300

    Complete features section

--- Frontend/src/components/utility/TitleBtn.jsx
+++ Frontend/src/components/utility/TitleBtn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TitleBtn = ({children}) => {
   return (
-    <button className="py-6 px-12 w-[165px] rounded-full bg-lightPurple text-darkPurple font-semibold text-lg text-center">{children}</button>
+    <button className="py-6 px-12  rounded-full bg-lightPurple text-darkPurple font-semibold text-lg text-center">{children}</button>
   )
 }
 
