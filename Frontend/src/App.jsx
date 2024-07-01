58f30b94e27938cd676c5e279c637e27b6198106
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2024-07-01 01:01:24 -0500

    adding dashboard routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,28 +11,26 @@ import DashboardLayout from "./components/Layout/DashboardPage/Layout";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* landing page */}
-          <Route index element={<HomePage />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Layout />}>
+                  {/* landing page */}
+                  <Route index element={<HomePage />} />
+                  <Route path="/contact" element={<ContactUs />} />
+                  <Route path="*" element={<NotFound />} />
+              </Route>
+              {/* Authentication  */}
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/login" element={<Login />} />
 
-          {/* Authentication  */}
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-
-          <Route path="/dashboard" element={<DashboardLayout />}>
-          {/* dashboard page */}
-          {/* <Route index element={<HomePage />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="*" element={<NotFound />} /> */}
-        </Route>
-      </Routes>
-      
-    </BrowserRouter>
+              <Route path="/dashboard" element={<DashboardLayout />}>
+              {/* dashboard page */}
+              {/* <Route index element={<HomePage />} />
+              <Route path="/contact" element={<ContactUs />} />
+              <Route path="*" element={<NotFound />} /> */}
+              </Route>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
