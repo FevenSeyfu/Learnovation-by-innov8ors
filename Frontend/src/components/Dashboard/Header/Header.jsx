5fdd1845d71753e3991b4b351493eee0831b5beb
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2024-07-01 01:41:04 -0500

    adding search bar

--- Frontend/src/components/Dashboard/Header/Header.jsx
+++ Frontend/src/components/Dashboard/Header/Header.jsx
@@ -1,39 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import searchIcon from "../../../assets/images/search_icon.png";
 
 const Header = () => {
     return (
-        <div>
-            <nav>
-                <ul className="flex flex-row justify-between">
+        <header className="bg-white py-4 shadow-md">
+        <div className="container mx-auto flex justify-between items-center">
+            <div>
+                
+                <input type="text"
+                src={searchIcon}
+                placeholder="Search"
+                className=""
+                ></input>
+            </div>
+            <nav className="flex items-center space-x-4">
+                  <ul className="flex space-x-4 text-lg font-medium items-center">
+                      <li>
+                          <Link to="/" className="flex items-center"> {/* Link for the logo */}
+                          <img src="/LearnovationAcademyLogo.png" alt="Learnnovation Academy Logo" className="h-20" /></Link>
+                      </li>
+                      <li>
+                          <Link to="/contact" className="text-gray-700 hover:text-gray-900">Contact</Link>
+                      </li>
+                  </ul>
 
-                    <Form inline>
-                        <FormControl
-                            onChange={this.handleSearchInput}
-                            value={this.state.searchText}
-                            type="text"
-                            placeholder="Search"
-                            className="mr-sm-2"
-                        />
-                        <Button onClick={this.handleSearchSubmit} variant="outline-info">
-                            Search
-                        </Button>
-                    </Form>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                    <li>
-                        <Link to="/signup">Sign up</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Log in</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+                  <ul className="flex space-x-8 text-lg font-medium items-center">
+
+                  </ul>
+
+                  <div className="flex items-center space-x-4"> {/* Increased spacing with space-x-8 */}
+                      <Link to="/signup"
+                              className="bg-purple hover:opacity-95 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                          Sign Up
+                      </Link>
+                      <Link to="/login"
+                              className="bg-white hover:bg-gray-100 text-gray-700 font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                            Log In
+                      </Link>
+                  </div>
+              </nav>
+          </div>
+      </header>
     );
 };
 
