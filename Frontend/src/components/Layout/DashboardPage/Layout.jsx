9f7c3c11d7218e0cf2cb388d300e86571d6615b0
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2024-07-01 03:09:29 -0500

    Included Left and Right Sidebar

--- Frontend/src/components/Layout/DashboardPage/Layout.jsx
+++ Frontend/src/components/Layout/DashboardPage/Layout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../../Dashboard/Header/Header";
-// import Footer from "../../LandingPage/Footer/Footer";
+import LeftSideBar from "../../Dashboard/Sidebar/LeftSideBar";
+import RightSideBar from "../../Dashboard/Sidebar/RightSideBar";
 
 const Layout = () => {
   return (
     <div >
+      
+      <LeftSideBar />
       <Header />
+      <RightSideBar />
       <main >
         <Outlet />
       </main>
