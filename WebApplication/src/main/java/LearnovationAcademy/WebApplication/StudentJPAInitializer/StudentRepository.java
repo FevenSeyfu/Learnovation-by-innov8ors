364d20a9dbe26291f9937c0f2a1ca5ec698e8d99
SumanthRam07 <sumanthgollaprolu07@gmail.com>
2024-07-02 17:03:51 -0400

    backend-commit/2-july-2024/2

---
+++ WebApplication/src/main/java/LearnovationAcademy/WebApplication/StudentJPAInitializer/StudentRepository.java
@@ -0,0 +1,13 @@
+package LearnovationAcademy.WebApplication.StudentJPAInitializer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import LearnovationAcademy.WebApplication.Student.Student;
+
+public interface StudentRepository  extends JpaRepository<Student, Integer>{
+	
+	
+	
+	Student findByUsername(String username) ; 
+
+}
