f7a05de16828b3b3e2026293f8a1370c05b73ed9
Ibe Ezenwa Augustine <austinibe15@gmail.com>
2024-07-02 08:28:17 -0700

    Add Agenda to the Component

---
+++ Frontend/src/App1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Agenda from './components/Agenda/Agenda/'; // Correct import path
+
+function App() {
+  return (
+    <div className="App1">
+      <Agenda />
+    </div>
+  );
+}
+
+export default App1;
