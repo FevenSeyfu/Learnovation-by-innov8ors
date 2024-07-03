f1fa110a3fcbb3e8c49a7969287e1666be4ac53d
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2024-07-02 23:42:30 -0500

    Added Dark Styling to Header & Agenda

--- Frontend/src/components/Dashboard/Header/Header.jsx
+++ Frontend/src/components/Dashboard/Header/Header.jsx
@@ -8,31 +8,40 @@ import profileImg from "../../../assets/images/dashboard-profile-example.png";
 
 const Header = () => {
     return (
-        <header className="bg-white py-4 shadow-md">
+        <header className="bg-white py-4 shadow-md dark:bg-gray-900 dark:text-white">
         <div className="container mx-auto flex justify-between items-center">
             <div>
                 <div class="relative">
                     <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <svg className="w-4 h-4 text-gray-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
                     </div>
-                    <input type="search" id="default-search" placeholder="Search" class="block w-full p-2 ps-10 text-sm border border-zinc-200 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    <input type="search" id="default-search" placeholder="Search" class="block w-full p-2 ps-10 text-sm border border-zinc-200 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-900 dark:border-slate-700 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                 </div>
             </div>
             <nav className="flex items-center space-x-4">
                   <ul className="flex space-x-4 text-lg font-medium items-center">
                       <li>
                           <Link to="/streak-tracker" className="flex items-center"> {/* Link for the logo */}
-                          <img src={bolt} alt="Bolt" className="" /><p id="streak">0</p></Link>
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke="currentColor" d="M3.99621 18H2.99621L3.99621 11H0.496207C-0.0837926 11 -0.0737927 10.68 0.116207 10.34C0.306207 10 0.166207 10.26 0.186207 10.22C1.47621 7.94 3.41621 4.54 5.99621 0H6.99621L5.99621 7H9.49621C9.98621 7 10.0562 7.33 9.96621 7.51L9.89621 7.66C5.95621 14.55 3.99621 18 3.99621 18Z" fill="white"/>
+                            </svg>
+                          <p id="streak">0</p></Link>
                       </li>
                       <li>
                         <Link to="/inbox" className="flex items-center"> {/* Link for the logo */}
-                        <img src={inbox} alt="Inbox" className="" /></Link>
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 13h3.439a.991.991 0 0 1 .908.6 3.978 3.978 0 0 0 7.306 0 .99.99 0 0 1 .908-.6H20M4 13v6a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-6M4 13l2-9h12l2 9"/>
+                            </svg>
+                        </Link>
                       </li>
                       <li>
                         <Link to="/dashboard" className="flex items-center"> {/* Link for the logo */}
-                        <img src={notification} alt="Notifications" className="" /></Link>
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5.365V3m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175 0 .593 0 1.292-.538 1.292H5.538C5 18 5 17.301 5 16.708c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 12 5.365ZM8.733 18c.094.852.306 1.54.944 2.112a3.48 3.48 0 0 0 4.646 0c.638-.572 1.236-1.26 1.33-2.112h-6.92Z"/>
+                            </svg>
+                        </Link>
                       </li>
                   </ul>
 
--- Frontend/src/components/Dashboard/Sidebar/Agenda.jsx
+++ Frontend/src/components/Dashboard/Sidebar/Agenda.jsx
@@ -13,11 +13,14 @@ const Agenda = () => {
   const { toggleRightSidebar } = useSidebar();
   const { isRightSidebarExpanded } = useSidebar();
   return (
-    <div className="h-screen border">
+    <div className="h-screen border dark:bg-gray-900 dark:text-white">
         <button onClick={toggleRightSidebar}>
         {(!isRightSidebarExpanded) && (
             <div className="p-6">
-              <img src={plansIcon} alt="Plans Icon" className=""></img>
+              <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 10h16M8 14h8m-4-7V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"/>
+              </svg>
+
             </div>
             
           )}
@@ -25,7 +28,9 @@ const Agenda = () => {
             <div className="p-5 mb-4">
               <ul>
                 <li className="flex items-center space-x-4">
-                  <img src={plansIcon} alt="Plans Icon" ></img>
+                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 10h16M8 14h8m-4-7V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"/>
+                  </svg>
                   <p className="font-medium text-xl">Your Plan</p>
                 </li>
               </ul>
@@ -36,23 +41,30 @@ const Agenda = () => {
       
       {isRightSidebarExpanded && (
         <div>
-        <div class="p-5 mb-4 border border-gray-100 rounded-lg  dark:bg-gray-800 dark:border-gray-700">
+        <div class="p-5 mb-4 border border-gray-100 rounded-lg  dark:bg-gray-800 dark:border-gray-900">
             <time class="text-2xl font-medium text-gray-900 dark:text-white">Today</time>
             <ol class="mt-3 divide-y divider-gray-200 dark:divide-gray-700">
                 <li>
                     <a href="#" class="items-center block p-3 sm:flex ">
                         <img class="w-24 h-24 mb-3 me-3 rounded-lg sm:mb-0" src={sasImg} alt="Analytic Screens"/>
                         <div class="text-gray-600 dark:text-gray-400">
-                            <p class="font-semibold text-lg">Strategic Analysis Showcase</p>
+                            <p class="font-semibold text-lg dark:text-white">Strategic Analysis Showcase</p>
 
                             <div class="text-xs font-normal">
                               <ul>
                                 <li className="flex items-center space-x-2">
-                                  <img src={calIcon} alt="Calendar Icon"></img>
+                                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z"/>
+                                  </svg>
                                   <time className="">September 10, 2024</time>
-                                  <img src={locIcon} alt="Location Icon"></img>
+                                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z"/>
+                                  </svg>
                                   <p className="">Virtual</p>
-                                  <img src={catIcon} alt="Category Icon"></img>
+                                  <svg class="w-6 h-6 text-gray-800 dark:text-white dark:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path stroke="currentColor" d="M315.4 15.5C309.7 5.9 299.2 0 288 0s-21.7 5.9-27.4 15.5l-96 160c-5.9 9.9-6.1 22.2-.4 32.2s16.3 16.2 27.8 16.2H384c11.5 0 22.2-6.2 27.8-16.2s5.5-22.3-.4-32.2l-96-160zM288 312V456c0 22.1 17.9 40 40 40H472c22.1 0 40-17.9 40-40V312c0-22.1-17.9-40-40-40H328c-22.1 0-40 17.9-40 40zM128 512a128 128 0 1 0 0-256 128 128 0 1 0 0 256z"/>
+                                  </svg>
                                   <p className="">Challenges</p>
                                 </li>
                               </ul>
@@ -88,7 +100,7 @@ const Agenda = () => {
                             <ul>
                                 <li className="flex items-center space-x-2">
                                   <div className="w-56">
-                                    <p className="text-xl font-medium">Course Goal</p>
+                                    <p className="text-xl font-medium dark:text-white">Course Goal</p>
 
                                     <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 font-semibold text-sm">
                                       <div className="bg-gradient-to-r from-violet-500 to-pink-500 h-2.5 rounded-full w-[36%]"> </div>36%
