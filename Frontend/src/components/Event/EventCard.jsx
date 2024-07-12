715303262b234d970b69822a81cad02985486da2
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2024-07-12 03:28:37 -0500

    added dark mode bg

--- Frontend/src/components/Event/EventCard.jsx
+++ Frontend/src/components/Event/EventCard.jsx
@@ -26,7 +26,7 @@ const EventCard = ({
     navigate(`/event/${id}`);
   };
   return (
-    <div className="font-inter rounded-lg p-4 border border-[#C8C7FF] bg-[#FCFCFD] dark:bg-gray-900 dark:text-white dark:border-neutral-400 flex flex-col gap-2 ">
+    <div className="font-inter rounded-lg p-4 border border-[#C8C7FF] bg-[#FCFCFD] text-gray-900 dark:text-white dark:bg-gray-800 dark:border-gray-700 flex flex-col gap-2 ">
       <img src={getImageURL(eventImg)} alt="event image" />
       <div className="max-h-8 overflow-hidden">
       <Typography tag={"h2"} weight={"semiBold"} size={"lg"} type={"text"}>
